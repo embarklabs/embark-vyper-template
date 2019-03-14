1e6554a509d7a4c94b1d89a61148c858fffce981
emizzle <ericmastro@status.im>
2019-03-14 16:45:13 +1100

    feat(@embark-vyper-template): Support Embark 4.0.0
    Support Embark 4.0.0.
    
    Bump version.

--- config/blockchain.js
+++ config/blockchain.js
@@ -14,17 +14,22 @@ module.exports = {
     rpcCorsDomain: "auto",  // Comma separated list of domains from which to accept cross origin requests (browser enforced)
                             // When set to "auto", Embark will automatically set the cors to the address of the webserver
     proxy: true, // Proxy is used to present meaningful information about transactions
-    account: {
-      // "address": "", // When specified, uses that address instead of the default one for the network
-      password: "config/development/password" // Password to unlock the account
-    },
+    accounts: [
+      {
+        nodeAccounts: {
+          password: "config/development/password" // Password to unlock the account
+        }
+      },
+      {
+        mnemonic: "example exile argue silk regular smile grass bomb merge arm assist farm"
+      }
+    ],
     targetGasLimit: 8000000, // Target gas limit sets the artificial target gas floor for the blocks to mine
     wsRPC: true, // Enable the WS-RPC server
     wsOrigins: "auto",  // Origins from which to accept websockets requests
                         // When set to "auto", Embark will automatically set the cors to the address of the webserver
     wsHost: "localhost", // WS-RPC server listening interface (default: "localhost")
     wsPort: 8546, // WS-RPC server listening port (default: 8546)
-    simulatorMnemonic: "example exile argue silk regular smile grass bomb merge arm assist farm", // Mnemonic  used by the simulator to generate a wallet
     simulatorBlocktime: 0 // Specify blockTime in seconds for automatic mining. Default is 0 and no auto-mining.
   },
   testnet: {
@@ -34,9 +39,13 @@ module.exports = {
     rpcHost: "localhost",
     rpcPort: 8545,
     rpcCorsDomain: "http://localhost:8000",
-    account: {
-      password: "config/testnet/password"
-    }
+    accounts: [
+      {
+        nodeAccounts: {
+          password: "config/testnet/password" // Password to unlock the account
+        }
+      }
+    ]
   },
   livenet: {
     enabled: true,
@@ -45,9 +54,13 @@ module.exports = {
     rpcHost: "localhost",
     rpcPort: 8545,
     rpcCorsDomain: "http://localhost:8000",
-    account: {
-      password: "config/livenet/password"
-    }
+    accounts: [
+      {
+        nodeAccounts: {
+          password: "config/livenet/password" // Password to unlock the account
+        }
+      }
+    ]
   },
   privatenet: {
     enabled: true,
